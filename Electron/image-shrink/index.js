41af929e747f75d3544224a448c36a89f114f021
Azim Uddin Ahamed <azim@sqbear.com.au>
2022-03-15 11:09:54 +0600

    configured the electron js with resizable

--- Electron/image-shrink/index.js
+++ Electron/image-shrink/index.js
@@ -1,10 +1,28 @@
 const { app, BrowserWindow } = require("electron")
 
+process.env.NODE_ENV = "development";
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform !== 'darwin' ? true : false;
+let mainWindow;
+console.log(isDev)
 const createMainWindow = () => {
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         title: "ImageShrink",
         height: 500,
-        width: 600
+        width: 600,
+        resizable: !isDev ? false : true,
+        icon: "./assets/icon.png",
     })
+    // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+    mainWindow.loadFile("./app/index.html")
 }
+app.on('window-all-closed', () => {
+    if (!isMac) app.quit()
+})
+app.whenReady().then(() => {
+    createMainWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
 app.on('ready', createMainWindow)
