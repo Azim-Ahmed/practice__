13ca2603a7f751be02080b579c3ce0898ae7f409
Azim Uddin Ahamed <azimuahmed13@gmail.com>
2022-11-23 16:16:27 +0600

    created a function for time complexity

--- data-structure-and-algorithm/timeOfOurCode.js
+++ data-structure-and-algorithm/timeOfOurCode.js
@@ -1,3 +1,15 @@
 //BigONotation
 
 //how do we know this is the best function to be for our problem solving
+
+//Data Structure And Algorithms #2 Big O Notation And Performance Optimization
+
+//1
+function sumAll(n) {
+    let total = 0;
+    for (let index = 0; index < n.length; index++) {
+        total += i;
+    }
+    return total
+}
+console.log(sumAll(10))
